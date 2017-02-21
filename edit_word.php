7bce824894f09dd78a266ebfd542d35c4e3a0d03
tylerThrash <tjthrash2112@outlook.com>
2017-02-21 14:11:55 -0600

    Changed names of title to these files.

--- edit_word.php
+++ edit_word.php
@@ -28,12 +28,12 @@ $english_in_gujarathi = $_POST['english_in_gujarathi'];
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-   <title>Assignment 5 - Edit Word</title>
+   <title>Midterm Project - Group 3</title>
   </head>
   <body>
 	<div class="container">
 	<div class="row">
-		<h1>Assignment 5 - Edit word</h1>	
+		<h1>Edit word</h1>	
 		<hr />
 		<form action="process_edit.php" method="post">
 			<table class="table table-striped">
--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $stmt->bind_result($topic, $image, $english, $telugu, $hindi, $kannada, $gujarat
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 	<link rel="stylesheet" href="styles/style.css" type="text/css">
-   <title>Assignment 5</title>
+   <title>Word Explorer Group 3</title>
   </head>
   <body>
 	<div class="container">
